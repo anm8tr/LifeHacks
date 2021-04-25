a01493fae17f0c645a4d805190d5659f60b988cc
Richard Garrison <threeduser@hotmail.com>
2021-04-24 21:38:31 -0700

    Initial Commit

---
+++ LifeHacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeHacks
+//
+//  Created by Richard Garrison on 4/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
