32209a3e289dde0d5ddbcdd8a2c7efd6b82fd30b
Richard Garrison <threeduser@hotmail.com>
2021-05-08 15:59:16 -0700

    Pass state between VC's, add segues

--- LifeHacks/View Controllers/EditProfileViewController.swift
+++ LifeHacks/View Controllers/EditProfileViewController.swift
@@ -12,16 +12,34 @@ class EditProfileViewController: UIViewController {
     @IBOutlet weak var nameTextField: RoundTextField!
     @IBOutlet weak var aboutMeTextView: RoundTextView!
     
+    var stateController: StateController?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let user = stateController?.user
+        nameTextField.text = user?.name
+        aboutMeTextView.text = user?.aboutMe
 
         
     }
     
     @IBAction func save(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
+        if let stateController = stateController, let name = nameTextField.text, let aboutMe =
+            aboutMeTextView.text, !name.isEmpty && !aboutMe.isEmpty {
+            let oldUser = stateController.user
+            stateController.user = User(name: name, aboutMe: aboutMe, profileImage: oldUser.profileImage, reputation: oldUser.reputation)
+            dismiss(animated: true, completion: nil)
+        } else {
+            let title = "Missing name or about me"
+            let message = "Both name and about me need to be present t be able to save your editing"
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(cancelAction)
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        
     }
     
     @IBAction func cancel(_ sender: UIButton) {
--- LifeHacks/View Controllers/ProfileViewController.swift
+++ LifeHacks/View Controllers/ProfileViewController.swift
@@ -15,16 +15,23 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var aboutMeLabel: UILabel!
     
     fileprivate let stateController = StateController()
+    //var stateController: StateController? = StateController()
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         let user = stateController.user
         profilePictureImageView.image = UIImage(named: user.profileImage)
         nameLabel.text = user.name
         reputationLabel.text = "\(user.reputation)"
         aboutMeLabel.text = user.aboutMe
     
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? EditProfileViewController {
+            destination.stateController = stateController
+        }
     }
     
 
